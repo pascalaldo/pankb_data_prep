4b0a67be6e099aba5d201501f3772ec43f21dbea
Pascal Pieters <pascalaldo@gmail.com>
2024-07-11 16:12:22 +0200

    Use seqfu info instead of gtdb

--- pankb_data_prep/family_summary.py
+++ pankb_data_prep/family_summary.py
@@ -16,6 +16,12 @@ def initialize_parser(parser):
         help="GTDB meta csv file.",
     )
     parser.add_argument(
+        "--seqfu",
+        type=str,
+        required=True,
+        help="SeqFu stats csv file.",
+    )
+    parser.add_argument(
         "--output",
         "-o",
         type=str,
@@ -24,14 +30,19 @@ def initialize_parser(parser):
     )
 
 
-def family_summary_table(family, gtdb_meta_path, family_summary_path):
+def family_summary_table(family, gtdb_meta_path, seqfu_stats_path, family_summary_path):
     if not family.startswith("f__"):
         family = f"f__{family}"
     df = pd.read_csv(gtdb_meta_path, low_memory=False, index_col=0).loc[
-        :, ["Family", "gc_percentage", "genome_size"]
+        :, ["Family"]
     ]
+    df_seqfu = pd.read_csv(seqfu_stats_path, header=0, index_col=0, low_memory=False).loc[
+        :, ["gc", "Total"]
+    ]
+    df = df.join(df_seqfu)
+    df.rename(columns={"gc": "gc_content", "Total": "genome_size"}, inplace=True)
     df["source"] = "ncbi"
-    df["gc_content"] = round((df["gc_percentage"]) * 0.01, 3)
+    # df["gc_content"] = round((df["gc_percentage"]) * 0.01, 3)
     df["genome_len"] = df["genome_size"].astype(int)
     df = df.loc[df["Family"] == family, :]
 
@@ -39,7 +50,7 @@ def family_summary_table(family, gtdb_meta_path, family_summary_path):
 
 
 def run(args):
-    family_summary_table(args.name, args.gtdb_meta, args.output)
+    family_summary_table(args.name, args.gtdb_meta, args.seqfu, args.output)
 
 
 def main():
--- pankb_data_prep/full_summary.py
+++ pankb_data_prep/full_summary.py
@@ -11,6 +11,12 @@ def initialize_parser(parser):
         help="GTDB meta csv file.",
     )
     parser.add_argument(
+        "--seqfu",
+        type=str,
+        required=True,
+        help="SeqFu stats csv file.",
+    )
+    parser.add_argument(
         "--output",
         "-o",
         type=str,
@@ -19,19 +25,24 @@ def initialize_parser(parser):
     )
 
 
-def full_summary_table(gtdb_meta_path, full_summary_path):
+def full_summary_table(gtdb_meta_path, seqfu_stats_path, full_summary_path):
     df = pd.read_csv(gtdb_meta_path, low_memory=False, index_col=0).loc[
-        :, ["Family", "gc_percentage", "genome_size"]
+            :, ["Family"]
     ]
+    df_seqfu = pd.read_csv(seqfu_stats_path, header=0, index_col=0, low_memory=False).loc[
+        :, ["gc", "Total"]
+    ]
+    df = df.join(df_seqfu)
+    df.rename(columns={"gc": "gc_content", "Total": "genome_size"}, inplace=True)
     df["source"] = "ncbi"
-    df["gc_content"] = round((df["gc_percentage"]) * 0.01, 3)
+    # df["gc_content"] = round((df["gc_percentage"]) * 0.01, 3)
     df["genome_len"] = df["genome_size"].astype(int)
 
     df.to_csv(full_summary_path)
 
 
 def run(args):
-    full_summary_table(args.gtdb_meta, args.output)
+    full_summary_table(args.gtdb_meta, args.seqfu, args.output)
 
 
 def main():
